49e90d2334d345cda713890d77d074de0645d48e
Hugo Gallien <hgallien@z3r7p1.42lyon.fr>
2022-05-18 17:31:39 +0200

    message to client working

--- test_DB_car/blog-backend/src/app.module.ts
+++ test_DB_car/blog-backend/src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './orm.config';
 import { CarModule } from './car/car.module';
+import { SocketGateway } from './socket.gateway';
 @Module({
   imports: [TypeOrmModule.forRoot(config), CarModule],
+  providers: [SocketGateway],
 })
 export class AppModule {}
