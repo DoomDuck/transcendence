aaabf245d0ab367c7bdd75e82ec0ba0bd85eb07b
Hugo Gallien <hgallien@z3r8p5.42lyon.fr>
2022-05-30 17:12:38 +0200

    plus rien ne marche, installation alertes

--- test_DB_car/blog-backend/src/app.module.ts
+++ test_DB_car/blog-backend/src/app.module.ts
@@ -4,8 +4,11 @@ import { config } from './orm.config';
 import { CarModule } from './car/car.module';
 import { SocketGateway } from './socket.gateway';
 import { ChatGateway } from './chat/chat.gateway';
+import { AlertGateway } from './alert/alert.gateway';
+import { AlertController } from './alert/alert.controller';
 @Module({
   imports: [TypeOrmModule.forRoot(config), CarModule],
-  providers: [SocketGateway, ChatGateway],
+  providers: [SocketGateway, ChatGateway, AlertGateway],
+  controllers: [AlertController],
 })
 export class AppModule {}
