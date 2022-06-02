f361ba3a6b90b4e2396197d5f003734fb4479de1
DoomDuck <docdoomduck@protonmail.com>
2022-06-02 18:20:47 +0200

    fix: Move styles back to global

--- src/front/Pong.svelte
+++ src/front/Pong.svelte
@@ -10,29 +10,3 @@ setup();
     <button id="launch-1">P2</button>
     <button id="launch-2">Spectator</button>
 </div>
-
-<style>
-
-body {
-    overflow: hidden;
-    margin: 0px;
-}
-canvas {
-    position: relative;
-    left: 50%;
-    top: 50%;
-    transform: translateX(-50%);
-}
-
-.game-text {
-    position: absolute;
-    top: 0px;
-}
-
-.player-score {
-    color: #FFF;
-    font-family: sans-serif;
-    background: rgba(0, 0, 0, 0);
-}
-
-</style>
