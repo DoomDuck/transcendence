64b7aac04ae7cf52af05dedd2af24be869fbd72f
DoomDuck <docdoomduck@protonmail.com>
2022-06-24 14:58:09 +0200

    feat: Add security to only remove hooks if it's link

--- scripts/hooks/remove.sh
+++ scripts/hooks/remove.sh
@@ -3,14 +3,24 @@
 # Quit on error
 set -e
 
+HOOK_PATH=.git/hooks/pre-commit
+
+check_hook_is_link() {
+  [[ ! -f $HOOK_PATH ]] && return 0
+  echo 1>&2 "Error: Hook script is not a link" && exit 1
+}
+
 remove_hook() {
-  rm  1>&2 2>/dev/null .git/hooks/pre-commit || return 1
+  rm 2>&1 >/dev/null $HOOK_PATH
+}
+
+success() {
   echo "Hook has been remove to install again run ./scripts/hooks/remove.sh"
 }
 
 show_help() {
-  echo 2>&1 "Could not remove hook!"
+  echo 2>&1 "Error: Could not remove hook!"
   echo 2>&1 "It might not exist or you might not be at the project root"
 }
 
-remove_hook || show_help
+check_hook_is_link && remove_hook && success || show_help
