36948fc647242ac14efc6e66b7e1c091cdaaf5a7
DoomDuck <docdoomduck@protonmail.com>
2022-07-11 14:50:14 +0200

    fix: Macos compatible maching

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-shopt -s globstar
-
 # Install prettier if missing
 if [[ ! -f node_modules/prettier/bin-prettier.js ]]; then
     npm install --no-save prettier
 fi
 
-exec ./node_modules/prettier/bin-prettier.js --check **/*.svelte **/*.ts
+exec ./node_modules/prettier/bin-prettier.js --check \
+    $(find src -name '*.ts' && find src -name '*.svelte')
