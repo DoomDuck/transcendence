c97e538f6b87c216f4d2916384a7cac3382147fb
Magic Emma <emmafarin@gmail.com>
2022-07-13 17:50:56 +0200

    changed spaces to tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import Menu from "./Menu.svelte";
-  import StartAGame from './StartAGame.svelte';
-  import Chat from './Chat.svelte';
-  import Play from './Play.svelte';
-  import Friends from './friendsList.svelte'
-  import Waiting from './waitingRoom.svelte'
+	import Menu from "./Menu.svelte";
+	import StartAGame from './StartAGame.svelte';
+	import Chat from './Chat.svelte';
+	import Play from './Play.svelte';
+	import Friends from './friendsList.svelte'
+	import Waiting from './waitingRoom.svelte'
 
-  type Status =  "Menu" | "StartAGame" | "Chat" | "Play" | "Friends" | "Waiting";
+	type Status = "Menu" | "StartAGame" | "Chat" | "Play" | "Friends" | "Waiting";
 
-  let status : Status = "Menu";
+	let status : Status = "Menu";
 
-  function go_to(place: Status) {
-    return () => status = place;
-  }
+	function go_to(place: Status) {
+		return () => status = place;
+	}
 
 </script>
 
@@ -24,23 +24,23 @@
 
 {#if status == "Menu"}
 <Menu
-  on:start_game={go_to("StartAGame")}
-  on:open_chat={go_to("Chat")}
+	on:start_game={go_to("StartAGame")}
+	on:open_chat={go_to("Chat")}
 />
 {:else if status == "StartAGame"}
 <StartAGame
-on:Play={go_to("Play")}
-on:open_menu={go_to("Menu")}
-on:see_friends={go_to("Friends")}
+	on:Play={go_to("Play")}
+	on:open_menu={go_to("Menu")}
+	on:see_friends={go_to("Friends")}
 />
 {:else if status == 'Play'}
 <Play
-on:start_game={go_to("StartAGame")}
+	on:start_game={go_to("StartAGame")}
 />
 {:else if status == 'Friends'}
 <Friends
-on:start_game={go_to("StartAGame")}
-on:start_waiting={go_to("Waiting")}
+	on:start_game={go_to("StartAGame")}
+	on:start_waiting={go_to("Waiting")}
 />
 {:else if status == 'Waiting'}
 <Waiting
@@ -48,6 +48,6 @@ on:start_waiting={go_to("Waiting")}
 />
 {:else}
 <Chat
-  on:open_menu={go_to("Menu")}
+	on:open_menu={go_to("Menu")}
 />
 {/if}
