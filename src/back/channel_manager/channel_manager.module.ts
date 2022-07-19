b27a137e85a3893ffe5da0d68c3609fe851acff8
Hugo Gallien <hgallien@z3r2p5.42lyon.fr>
2022-07-19 12:47:34 +0200

    channel manager removed

--- src/back/channel_manager/channel_manager.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ChannelManagerService } from "./channel_manager.service";
-
-@Module({
-  providers: [ChannelManagerService],
-})
-export class ChannelManagerModule {}
