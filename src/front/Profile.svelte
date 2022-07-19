de79b953557ae6189687759d84eb1f4efb8441ee
Magic Emma <emmafarin@gmail.com>
2022-07-19 16:49:59 +0200

    flex box for Profile

--- src/front/Profile.svelte
+++ src/front/Profile.svelte
@@ -1,23 +1,38 @@
 <script lang="ts">
   export let image: string;
   export let friendName: string;
+  export let iD = 35214;
 </script>
 
-<img class="roundedImageProfile" src={image} alt="profilepic" />
-<br />
-<h1>{friendName}</h1>
+<div id="Profile">
+  <img class="roundedImageProfile" src={image} alt="profilepic" />
+  <h3>{friendName}</h3>
+  <p> iD: {iD}</p>
+  
+</div>
 
 <style>
-  h1 {
+  #Profile{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  h3 {
     color: #ff00b8;
-    font-size: 1em;
     text-align: center;
+    line-height: 50%;
+  }
+  p {
+    color: #c9c7c7;
+    text-align: center;
+    font-size: smaller;
+    line-height: 50%;
   }
   .roundedImageProfile {
     overflow: hidden;
-    border-radius: 6em;
-    width: 8em;
-    height: 8em;
+    border-radius: 4em;
+    width: 6em;
+    height: 6em;
     margin-top: 2%;
     margin-left: auto;
     margin-right: auto;
