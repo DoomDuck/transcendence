c4a4bcd46edad88ae77c66a2b496cdd5e18c7304
Magic Emma <emmafarin@gmail.com>
2022-07-24 19:12:18 +0200

    changed way to have convChat

---
+++ front/src/lib/ConvChat.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+import { beforeUpdate, afterUpdate } from 'svelte';
+
+	type Comment = {
+		author: string,
+		text: string,
+		placeholder?: true,
+	};
+
+	let div: HTMLDivElement;
+	let autoscroll: boolean;
+
+	beforeUpdate(() => {
+		autoscroll = div && (div.offsetHeight + div.scrollTop) > (div.scrollHeight - 20);
+	});
+
+	afterUpdate(() => {
+		if (autoscroll) div.scrollTo(0, div.scrollHeight);
+	});
+
+	let comments : Comment[] = [
+		{ author: 'bot', text: "Hello !" }
+	];
+
+	function handleKeydown(event: KeyboardEvent) {
+		const inputElement = (event.target as HTMLInputElement);
+		if (event.key === 'Enter') {
+			const text = inputElement.value;
+			if (!text) return;
+
+			comments = comments.concat({
+				author: 'user',
+				text,
+			});
+
+			inputElement.value = '';
+
+			const reply = text + '!';
+
+			setTimeout(() => {
+				comments = comments.concat({
+					author: 'bot',
+					text: '...',
+					placeholder: true
+				});
+
+				setTimeout(() => {
+					comments = comments.filter(comment => !comment.placeholder).concat({
+						author: 'bot',
+						text: reply
+					});
+				}, 500 + Math.random() * 500);
+			}, 200 + Math.random() * 200);
+		}
+	}
+</script>
+
+<style>
+	.chat {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		max-width: 320px;
+	}
+
+	.scrollable {
+		flex: 1 1 auto;
+		border-top: 1px solid #eee;
+		margin: 0 0 0.5em 0;
+		overflow-y: auto;
+	}
+
+	article {
+		margin: 0.5em 0;
+	}
+
+	.user {
+		text-align: right;
+	}
+
+	span {
+		padding: 0.5em 1em;
+		display: inline-block;
+	}
+
+	.bot span {
+		background-color: #eee;
+		border-radius: 1em 1em 1em 0;
+	}
+
+	.user span {
+		background-color: #0074D9;
+		color: white;
+		border-radius: 1em 1em 0 1em;
+		word-break: break-all;
+	}
+</style>
+
+<div class="chat">
+	<div class="scrollable" bind:this={div}>
+		{#each comments as comment}
+			<article class={comment.author}>
+				<span>{comment.text}</span>
+			</article>
+		{/each}
+	</div>
+
+	<input on:keydown={handleKeydown}>
+</div>
