bb804c30762653a4658e6b8986ebcc18ff4d7ba8
DoomDuck <docdoomduck@protonmail.com>
2022-07-27 18:35:06 +0200

    fix: Remove console.log from user.controller

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -24,7 +24,6 @@ export class UserController {
   constructor(private userService: UserService) {}
   @Get()
   async getUser() {
-    console.log('Hello');
     return this.userService.findAllDb();
   }
   @Post()
