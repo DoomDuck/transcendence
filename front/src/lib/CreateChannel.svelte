43d879ee98173af5656c58cdba86f41783496aed
Magic Emma <emmafarin@gmail.com>
2022-07-27 16:17:52 +0200

    doing channel stuff

--- front/src/lib/CreateChannel.svelte
+++ front/src/lib/CreateChannel.svelte
@@ -1,4 +1,16 @@
-<img src="CreateChannel.png" alt="Create a channel" width=50px height=50px>
+<script lang='ts'>
+	import Modal from './Modal.svelte'
+
+	let createChan = false;
+</script>
+
+<img src="CreateChannel.png" alt="Create a channel" width=50px height=50px on:click={() => createChan = true }>
+
+{#if createChan}
+	<Modal on:close={() => {createChan = false}}>
+		
+	</Modal>
+{/if}
 
 <style>
 	img{
