645999a907dbafacc714f9f2814f13dba6b9163f
Magic Emma <emmafarin@gmail.com>
2022-07-28 22:07:46 +0200

    GameInvit obj to choose mode and invit friend to play

---
+++ front/src/lib/GameInvit.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+	import Switch from './Switch.svelte'
+	export let friendName: string;
+</script>
+
+<div id='invitation'>
+	<Switch optionOne='Classic Mode' optionTwo='Special Mode'/>
+	<a sveltekit:prefetch href="/WaitingRoom">
+		<button>
+			Invit {friendName} to play
+		</button>
+	</a>
+
+</div>
+
+<style>
+	#invitation{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 20px;
+		padding: 15px;
+	}
+</style>
