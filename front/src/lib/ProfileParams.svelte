011a733b821697f60cb76d826aa158ae5e7aeaff
Magic Emma <emmafarin@gmail.com>
2022-07-30 21:07:12 +0200

    start of parameters

---
+++ front/src/lib/ProfileParams.svelte
@@ -0,0 +1,74 @@
+<script lang='ts'>
+	export let avatar: string;
+	export let name: string;
+	let fileinput;
+	let changeName = false;
+	const onFileSelected =(e)=>{
+		let image = e.target.files[0];
+		let reader = new FileReader();
+
+		reader.readAsDataURL(image);
+		reader.onload = e => {
+			avatar = e.target.result
+		};
+	}
+</script>
+
+<div id="profileComponents">
+	<div id='profilePicture'>
+		{#if avatar}
+			<img class="avatar" src={avatar} alt="d" />
+		{:else}
+			<img class="avatar" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="" /> 
+		{/if}
+		<img class="upload" src="upload.png" alt="upload avatar" on:click={()=>{fileinput.click();}} />
+		<div class="chan" on:click={()=>{fileinput.click();}}></div>
+		<input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+	</div>
+	{#if !changeName}
+		<h1 on:click={() => (changeName = true)}>{name}</h1>
+	{:else}
+		<div>
+			<input bind:value={name} placeholder={name}/>
+			<img src='check.png' alt='confirm new name' width=30 height=30 on:click={() => (changeName = false)}/>
+		</div>
+	{/if}
+</div>
+
+<style>
+	input{
+		background-color: transparent;
+		color: rgb(255, 0, 191);
+	}
+	#profileComponents{
+		display:flex;
+		flex-direction:row;
+		gap: 5vw;
+		justify-content: center;
+		align-items:center;
+		width: 50vw;
+		height: 200px;
+		margin-top: 100px;
+		border: 1px solid #ff00b8;
+	}
+	#profilePicture{
+		display:flex;
+		justify-content: center;
+		align-items: center;
+		flex-flow:column;
+	}
+	.upload{
+		display:flex;
+		height:30px;
+		width:30px;
+		cursor:pointer;
+	}
+	.avatar{
+		display:flex;
+		overflow: hidden;
+		border-radius: 50%;
+		width: 100px;
+		height: 100px;
+		margin: 10px;
+	}
+</style>
---
+++ front/src/routes/Parameters.svelte
@@ -0,0 +1,74 @@
+<script lang='ts'>
+	import ProfileParams from '$lib/ProfileParams.svelte';
+	export let avatar: string;	
+	export let name= "Ted";
+	export let blocked = [{profilePic: 'cars.jpeg', name: "Flash McQueen"}];
+
+</script>
+
+<div id="app">
+	<ProfileParams {avatar} {name}/>
+	<div id='blockedProfiles'>
+		<div class='blocked'>
+			<div class='blockedInfos'>
+				{#each blocked as {profilePic, name }}
+					<img class="roundedImage" src={profilePic} alt="contact" />
+					<h3>{name}</h3>
+				{/each}
+			</div>
+			<button> unblock </button>
+		</div>
+	</div>
+</div>
+
+<style>
+	*{
+		font-family: 'Lato', sans-serif;
+		color: #06bcff;
+	}
+	
+	#app{
+		background-image: url('starsSky.jpeg');
+		width: 100%;
+		height: 100%;
+		overflow-y: scroll;
+		display:flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	#blockedProfiles{
+		width: 50vw;
+		height: 600px;
+		overflow-y: auto;
+		overflow-x: hidden;
+		display:flex;
+		flex-direction: column;
+		border: 1px solid #ff00b8;
+	}
+	.blocked{
+		width: 50vw;
+		height: 100px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.roundedImage {
+		overflow: hidden;
+		-webkit-border-radius: 50px;
+		-moz-border-radius: 50px;
+		border-radius: 30px;
+		width: 60px;
+		height: 60px;
+		margin: 20px;
+	}
+	.blockedInfos{
+		display:flex;
+		flex-direction: row;
+		justify-content: right;
+		align-items: center;
+	}
+	button{
+		margin:20px;
+	}
+</style>
