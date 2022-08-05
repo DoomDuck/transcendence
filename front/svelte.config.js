6f72e2741c3f050d1a33c6dd3d67737c940e3b72
DoomDuck <docdoomduck@protonmail.com>
2022-08-05 17:40:30 +0200

    fix: front/ format

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -3,20 +3,20 @@ import autoPreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  preprocess: autoPreprocess(),
-  kit: {
-      adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    }),
+	preprocess: autoPreprocess(),
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 
-    prerender: {
-      // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
-    },
-  }
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
+	}
 };
