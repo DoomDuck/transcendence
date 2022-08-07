872aa55c8d62970156a7531a2208a3499b6cdb73
DoomDuck <docdoomduck@protonmail.com>
2022-08-07 11:48:36 +0200

    feat: SocketAdapter

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,8 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+
+export class SocketAdapter extends IoAdapter {
+  createIOServer(
+    port: number,
+    options?: ServerOptions & {
+      namespace?: string;
+      server?: any;
+    },
+  ) {
+    const server = super.createIOServer(port, { ...options, cors: true });
+    return server;
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useWebSocketAdapter(new SocketAdapter(app));
   app.enableCors({ origin: true });
   await app.listen(5000);
 }
