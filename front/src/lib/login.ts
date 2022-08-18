34f5e4e686e898a0a981b4c7ffd9905bdbf1367d
DoomDuck <docdoomduck@protonmail.com>
2022-08-18 09:40:11 +0200

    feat: new Login route

--- front/src/lib/login.ts
+++ front/src/lib/login.ts
@@ -1,10 +1,6 @@
 import { io } from 'socket.io-client';
-// import { PUBLIC_APP_42_ID } from '$env/static/public'; 
-import { PUBLIC_APP_42_ID } from '$env/static/public';
 import { goto } from '$app/navigation';
 import type { ChatSocket } from '$lib/utils';
-
-const LOCATION = 'https://api.intra.42.fr/oauth/authorize';
 
 let socket : ChatSocket | null = null;
 
@@ -23,9 +19,6 @@ export async function login(): Promise<void> {
 		await goto("/Main");
 		return;
 	}
-	alert(`result: ${PUBLIC_APP_42_ID}`);
-	// const redirect_url = encodeURIComponent(window.location.origin);
-	// const url = `${LOCATION}?client_id=${env.PUBLIC_APP_42_ID}&redirect_uri=${redirect_url}&response_type=code`;
-	// window.history.pushState({}, '');
-	// window.location.assign(url);
+	window.history.pushState({}, '');
+	window.location.assign('http://localhost:5000/login');
 }
