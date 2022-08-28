d689ca851a47040be2c0efb5de7c740fbe6608a3
Magic Emma <emmafarin@gmail.com>
2022-08-28 16:54:44 +0200

    prettier 'sart a game' menu

--- front/src/routes/StartAGame/+page.svelte
+++ front/src/routes/StartAGame/+page.svelte
@@ -16,25 +16,41 @@
 		goto('/ChooseGameMode');
 	}
 </script>
-
-<h4><div on:click={() => play(true, false, true)}>Play Online</div></h4>
-<h4><div on:click={() => play(false)}>Play Offline</div></h4>
-<h4><div on:click={() => play(true, true)}>Observe</div></h4>
-<h4><a href="/FriendsList"> Challenge a friend </a></h4>
-<h4><a href="/Leaderboard"> Leaderboard </a></h4>
+<div id="main">
+	<div id="startGame">
+		<h4><div on:click={() => play(true, false, true)}>Play Online</div></h4>
+		<h4><div on:click={() => play(false)}>Play Offline</div></h4>
+		<h4><div on:click={() => play(true, true)}>Observe</div></h4>
+		<h4><a href="/FriendsList"> Challenge a friend </a></h4>
+		<h4><a href="/Leaderboard"> Leaderboard </a></h4>
+	</div>
+</div>
 
 <style>
-	h4 {
-		line-height: 300%;
+	#main{
+		width: 100%;
+		height: 100%;
+		display:  flex;
+		align-items: center;
+		justify-content: center;
 	}
-
+	#startGame{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+	h4{
+		font-size: 1.7em;
+	}
 	h4:hover {
 		background-color: blue;
 		width: 100%;
 		text-align: center;
 	}
 
-	a {
+	a{
 		all: unset;
 	}
+
 </style>
