9416fc758a84e0b57dd05be92a7d06e55e9a2be3
Magic Emma <emmafarin@gmail.com>
2022-09-04 10:15:55 +0200

    buttons style

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -59,4 +59,24 @@
 	:global(a) {
 		all: unset;
 	}
+	:global(button) {
+		background-color: #f9fdfe;
+		border: 1px solid #f8f9fa;
+		border-radius: 4px;
+		color: #3c4043;
+		cursor: pointer;
+		font-family: arial,sans-serif;
+		text-align: center;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
+		white-space: pre;
+		border-color: #dadce0;
+		box-shadow: rgba(0, 0, 0, .1) 0 1px 1px;
+		color: #999999;
+	}
+	:global(button:focus) {
+		border-color: #4285f4;
+		outline: none;
+	}
 </style>
